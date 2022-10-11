8c50576063c77e8de7cc319f12e068bf0e308a86
mfauzanmirfaqoh88 <mfauzanmirfaqoh88@gmail.com>
2022-10-11 20:41:31 +0700

    Intent Fauzan
    Belajar Intent

---
+++ app/src/main/java/com/fauzan/intentfauzan/MainActivity.kt
@@ -0,0 +1,71 @@
+package com.fauzan.intentfauzan
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var btnMoveActivity: Button
+    private lateinit var btnMoveWithData: Button
+    private lateinit var btnDialNumber: Button
+    private lateinit var btnResultFromActivity: Button
+    private lateinit var tvResult: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnMoveActivity = findViewById(R.id.btn_move_activity)
+        btnMoveActivity.setOnClickListener(this)
+
+        btnMoveWithData = findViewById(R.id.btn_move_with_data)
+        btnMoveWithData.setOnClickListener(this)
+
+        btnDialNumber = findViewById(R.id.btn_dial_number)
+        btnDialNumber.setOnClickListener(this)
+
+        btnResultFromActivity = findViewById(R.id.btn_result_from_activity)
+        btnResultFromActivity.setOnClickListener(this)
+
+        tvResult = findViewById(R.id.tv_result)
+        WarnaRecived()
+    }
+
+    private fun WarnaRecived() {
+        val bundle = intent.extras
+        val warna = bundle?.getString("Warna")
+        tvResult.text = warna
+    }
+
+    override fun onClick(v: View?) {
+        if (v != null) {
+            when (v.id) {
+                R.id.btn_move_activity -> run {
+                    val intent = Intent(this, MoveActivity::class.java)
+                    startActivity(intent)
+                }
+                R.id.btn_move_with_data -> run {
+                    val intent = Intent(this, MoveWithDataActivity::class.java)
+                    val bundle = Bundle()
+                    bundle.putString("Nama", "Fauzan")
+                    bundle.putString("Alamat", "Sidoarjo")
+                    intent.putExtras(bundle)
+                    startActivity(intent)
+                }
+                R.id.btn_dial_number -> run {
+                    var dialNumber ="083129335453"
+                    val intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel" + dialNumber))
+                    startActivity(intent)
+                }
+                R.id.btn_result_from_activity -> {
+                    val intent = Intent(this, MoveForResultActivity::class.java)
+                    startActivity(intent)
+                }
+            }
+        }
+    }
+}
